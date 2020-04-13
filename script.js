f35c73c29b949facd929a71965e6f501e8921986
Maxime Poitoux <poitouxmaxime@hotmail.fr>
2020-04-13 15:46:59 +0200

    update2

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-// // This function loads pokemon data from the Pokemon API
-// function fetchSimpsonsQuotesJSON() {
-//     // Feel free to download this HTML and edit it, to use another Pokemon ID
-//     const pokemonId = 1;
-//     const url = `https://simpsons-quotes-api.herokuapp.com/quotes${simpsonsId}`;
-//     axios.get(url)
-//       .then(function(response) {
-//         return response.data; // response.data instead of response.json() with fetch
-//       })
-//       .then(function(simpsons) {
-//         console.log('data decoded from JSON:', simpsons);
-  
-//         // Build a block of HTML
-//         const simpsonsHtml = `
-//           <p><strong>${simpsons.quote}</strong></p>
-//           <img src="${simpsons.character}" />
-//         `;
-//         document.querySelector('#simpsons').innerHTML = simpsonsHtml;
-//       });
-//   }
-  
-//   fetchSimpsonsQuotesJSON();
+
+// This function loads pokemon data from the Pokemon API
+function fetchSimpsonsQuotesJSON() {
+// Feel free to download this HTML and edit it, to use another Pokemon ID
+const url = `https://simpsons-quotes-api.herokuapp.com/quotes`;
+axios.get(url)
+    .then(function(response) {
+    return response.data; // response.data instead of response.json() with fetch
+    })
+    .then(function(simpsons) {
+    console.log('data decoded from JSON:', simpsons);
+
+    // Build a block of HTML
+    const simpsonsHtml = `
+        <p><strong>${simpsons[0].character}</strong></p>
+        <img src="${simpsons[0].image}" />
+        <p><strong>${simpsons[0].quote}</strong></p>
+    `;
+    document.querySelector('#simpsons').innerHTML = simpsonsHtml;
+    });
+}
+
+fetchSimpsonsQuotesJSON();
